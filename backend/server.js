51e7d7556a19ed92c8e379155ddae094a0a78c0a
Yash Raj Singh <yashrajsingh4799@gmail.com>
2022-11-28 12:12:50 +0530

    component structure created

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const socketIO = require("socket.io");
 const app=express();
 const port=4500 || process.env.PORT ;
 
+app.use(cors());
 app.get("/",(req,res)=>{
     res.send("HELL ITS WORKING");
 })
@@ -13,6 +14,26 @@ app.get("/",(req,res)=>{
 const server=http.createServer(app);
 
 const io=socketIO(server);
+
+io.on("connection",(socket)=>{
+    console.log("New Connection");
+
+    socket.on('joined',({user})=>{
+          users[socket.id]=user;
+          console.log(`${user} has joined `);
+          socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
+          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
+    })
+
+    socket.on('message',({message,id})=>{
+        io.emit('sendMessage',{user:users[id],message,id});
+    })
+
+    socket.on('disconnect',()=>{
+          socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
+        console.log(`user left`);
+    })
+});
 
 server.listen(port,()=>{
     console.log(`Working on http://localhost:${port}`);
